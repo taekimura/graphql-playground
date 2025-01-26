394c1d374552f7d3abd4ad2e50bcf26b17389cc6
Tae Kimura <sheepman7893@gmail.com>
2025-01-25 21:53:49 -0800

    fix: add mutation

--- src/server.js
+++ src/server.js
@@ -1,21 +1,40 @@
 const { ApolloServer, gql } = require('apollo-server');
+const fs = require('fs');
+const path = require('path');
 
-//　GraphQLのスキーマ定義
-const typeDefs = gql`
-  type Query {
-    info: String!
+// HackedNewsの投稿
+let links = [
+  {
+    id: 'link-0',
+    description: 'description for test.',
+    url: 'https://github.com/taekimura'
   }
-`;
+];
 
 //　resolver関数
 const resolvers = {
   Query: {
-    info: () => 'HackerNews Clone'
+    info: () => 'HackerNews Clone',
+    feed: () => links
+  },
+  Mutation: {
+    post: (parent, args) => {
+      let idCount = links.length;
+
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      };
+
+      links.push(link);
+      return link;
+    }
   }
 };
 
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf-8'),
   resolvers
 });
 
