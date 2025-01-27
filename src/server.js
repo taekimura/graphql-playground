1cd3ae917e82235df78f64aa2ca820761252f5a3
Tae Kimura <sheepman7893@gmail.com>
2025-01-26 20:37:01 -0800

    fix: test prisma studio

--- src/server.js
+++ src/server.js
@@ -2,40 +2,36 @@ const { ApolloServer, gql } = require('apollo-server');
 const fs = require('fs');
 const path = require('path');
 
-// HackedNewsの投稿
-let links = [
-  {
-    id: 'link-0',
-    description: 'description for test.',
-    url: 'https://github.com/taekimura'
-  }
-];
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 //　resolver関数
 const resolvers = {
   Query: {
     info: () => 'HackerNews Clone',
-    feed: () => links
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany();
+    }
   },
   Mutation: {
-    post: (parent, args) => {
-      let idCount = links.length;
-
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      };
-
-      links.push(link);
-      return link;
+    post: (parent, args, context) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description
+        }
+      });
+      return newLink;
     }
   }
 };
 
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf-8'),
-  resolvers
+  resolvers,
+  context: {
+    prisma
+  }
 });
 
 server.listen().then(({ url }) => console.log(`${url} server is running...`));
